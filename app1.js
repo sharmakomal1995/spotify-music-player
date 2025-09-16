ae08bd3b3bd7ab48650f1a87d040b93c4cd4942b
sharmakomal1995 <komal03sharma@gmail.com>
2025-09-16 15:08:38 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,14 @@
+
+let p = document.querySelector("p");
+
+p.addEventListener("click", function () {
+    console.log("para was clicked");
+});
+
+
+// let form = document.querySelector("form");
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     alert("form submitted");
+// });
